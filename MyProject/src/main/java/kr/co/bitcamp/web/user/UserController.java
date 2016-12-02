5b781d6e8b4776e7949851ca2aef0c87635da1f3
TurnUpYo <d_hboy@naver.com>
2016-12-02 10:32:28 +0900

    '.'

--- MyProject/src/main/java/kr/co/bitcamp/web/user/UserController.java
+++ MyProject/src/main/java/kr/co/bitcamp/web/user/UserController.java
@@ -1,8 +1,12 @@
 package kr.co.bitcamp.web.user;
 
+import java.io.File;
+import java.util.Iterator;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.ibatis.annotations.ResultType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -13,6 +17,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import kr.co.bitcamp.service.domain.Activity;
 import kr.co.bitcamp.service.domain.User;
@@ -181,8 +188,47 @@ public class UserController {
         
         return "";
       }
+      
+      @RequestMapping(value = "fileUpload/post", method=RequestMethod.POST) //ajax에서 호출하는 부분
+      //@ResponseBody 이거 쓰면 멘트뿌려줄수있음.
+      public String upload(MultipartHttpServletRequest multipartRequest) { //Multipart로 받는다.
+        
+        System.out.println("들왓니?");
+        
+          Iterator<String> itr =  multipartRequest.getFileNames();
 
-    
+          String filePath = "C:/test"; //설정파일로 뺀다.
+           
+          while (itr.hasNext()) { //받은 파일들을 모두 돌린다.
+               
+              /* 기존 주석처리
+              MultipartFile mpf = multipartRequest.getFile(itr.next());
+              String originFileName = mpf.getOriginalFilename();
+              System.out.println("FILE_INFO: "+originFileName); //받은 파일 리스트 출력'
+              */
+               
+              MultipartFile mpf = multipartRequest.getFile(itr.next());
+        
+              String originalFilename = mpf.getOriginalFilename(); //파일명
+        
+              String fileFullPath = filePath+"/"+originalFilename; //파일 전체 경로
+        
+              try {
+                  //파일 저장
+                  mpf.transferTo(new File(fileFullPath)); //파일저장 실제로는 service에서 처리
+                   
+                  System.out.println("originalFilename => "+originalFilename);
+                  System.out.println("fileFullPath => "+fileFullPath);
+        
+              } catch (Exception e) {
+                  System.out.println("postTempFile_ERROR======>"+fileFullPath);
+                  e.printStackTrace();
+              }
+                            
+         }
+            
+          return "Upload Success";
+      }
     
     
     
